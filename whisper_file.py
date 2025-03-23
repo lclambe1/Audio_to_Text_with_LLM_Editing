423d0eb1b4feab6c4867823dd07e7b140613edd6
lee.lambert <lee.lambert.2@au.af.edu>
2025-03-23 14:36:36 -0400

    Updated code with better naming conventions and made it so that the recording wasn't copied again into the recording_text_files

--- whisper_file.py
+++ whisper_file.py
@@ -8,8 +8,6 @@ It is intended to be used to transcribe audio files and then convert the .txt fi
 From here, a LLM will be used to edit the .docx file to make it more readable.
 """
 
-# STEP 1: Transcribe audio file to text; pulled from https://github.com/openai/whisper/blob/main/README.md
-
 model = whisper.load_model("turbo") #load whisper model
 recordings = "./Recordings" #path to audio file
 recording_text_files = "./Recording_text_files" #path to save text files
@@ -18,14 +16,14 @@ recording_titles = os.listdir(recordings) #list of audio files
 
 for item in recording_titles:
     story_path = os.path.join(recordings, item)
+    
+    # STEP 1: Transcribe audio file to text; pulled from https://github.com/openai/whisper/blob/main/README.md
     result = model.transcribe(story_path)
     
-    #create a file to save the text
-    os.makedirs(f"recording_text_files/{item}", exist_ok=True)
-    
-    #write the text to a .txt file
-    with open(f"recording_text_files/{item}.txt", "w") as f:
+    # STEP 2: write the text to a .txt file
+    name = item.split(".")[0]
+    with open(f"recording_text_files/{name}.txt", "w") as f:
         f.write(result["text"])
-        
+    
 
 #STEP 2: Take that text and write it to a .txt file
