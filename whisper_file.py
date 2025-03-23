3d08864dda725ab6b750e2cf73b9e453e9e7daee
lee.lambert <lee.lambert.2@au.af.edu>
2025-03-22 22:29:42 -0400

    Updated to take multiple files and transcribe them

--- whisper_file.py
+++ whisper_file.py
@@ -1,4 +1,5 @@
 import whisper
+import os
 
 """
 This project is used to transcribe audio files to text using the OpenAI Whisper model.
@@ -8,6 +9,23 @@ From here, a LLM will be used to edit the .docx file to make it more readable.
 """
 
 # STEP 1: Transcribe audio file to text; pulled from https://github.com/openai/whisper/blob/main/README.md
-model = whisper.load_model("turbo")
-result = model.transcribe("./Sample.m4a")
-print(result["text"])
+
+model = whisper.load_model("turbo") #load whisper model
+recordings = "./Recordings" #path to audio file
+recording_text_files = "./Recording_text_files" #path to save text files
+
+recording_titles = os.listdir(recordings) #list of audio files
+
+for item in recording_titles:
+    story_path = os.path.join(recordings, item)
+    result = model.transcribe(story_path)
+    
+    #create a file to save the text
+    os.makedirs(f"recording_text_files/{item}", exist_ok=True)
+    
+    #write the text to a .txt file
+    with open(f"recording_text_files/{item}.txt", "w") as f:
+        f.write(result["text"])
+        
+
+#STEP 2: Take that text and write it to a .txt file
