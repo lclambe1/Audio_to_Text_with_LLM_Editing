f863b6aa2a5a1a556863c1dba62895555f089f8e
lee.lambert <lee.lambert.2@au.af.edu>
2025-03-23 15:44:42 -0400

    Updated with the LLM mistral and llama3.2. It works well but the prompt should be changed so that it doesn't overdo changes

--- whisper_file.py
+++ whisper_file.py
@@ -2,6 +2,13 @@ import whisper
 import os
 from tqdm import tqdm
 
+import ollama
+from ollama import chat
+from ollama import ChatResponse
+
+import subprocess
+import time
+
 """
 This project is used to transcribe audio files to text using the OpenAI Whisper model.
 It uses a virtual environment with the requirements installed there.
@@ -36,6 +43,16 @@ def write_text_to_file(item, recording_text_files):
     with open(f"{recording_text_files}/{name}.txt", "w") as f:
         f.write(result["text"])
 
+def correct_grammar(result):
+    response: ChatResponse = chat(model='llama3.2', messages=[
+    {
+        'role': 'user',
+        'content': f'Please correct the grammar in the {result} text and reword so that it flows better as a story to captivate and to engage its reader.',
+    },
+    ])
+    #print(response['message']['content'])
+    # or access fields directly from the response object
+    print(response.message.content)
 
 
 if __name__ == "__main__":
@@ -45,14 +62,38 @@ if __name__ == "__main__":
     recording_text_files = "./Recording_text_files" #path to save text files
 
     recording_titles = os.listdir(recordings) #list of audio files
+    
+    # NOTE: The Llama model is not available in the virtual environment and must be downloaded. 
+    # Mistral (or whatever other model you choose) must be downloaded as well.
+    # Please see: https://github.com/ollama/ollama-python/blob/main/README.md for more information.
+    
+    # in Windows I had to run the following commands in the Powershell to run/install the Llama model:
+    # ollama serve
+    # ollama pull mistral
+    # ollama pull llama3.2
+    
+    # Load the Llama model
+    ollama_model = 'mistral'
+
+    try:
+        ollama.chat(ollama_model)
+        print('Model is available and running.')
+    except ollama.ResponseError as e:
+        print('Error:', e.error)
+        if e.status_code == 404:
+            ollama.pull(ollama_model)
+
 
     for item in tqdm(recording_titles):
         story_path = os.path.join(recordings, item)
         
         # STEP 1: transcribe the audio
         result = transcribe_audio(model, story_path)
+        print("Congratulations! Your audio has been transcribed.")
         
         # STEP 2: write the text to a .txt file
         write_text_to_file(item, recording_text_files)
+        print("Your text has been written to a .txt file.")
         
-        # STEP 3: convert the .txt file to a .docx file
+        # STEP 3: correct the grammar
+        correct_grammar(result)
