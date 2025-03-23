1ccbe9cb9585f0e0f9f321d63274764f523d18ac
lee.lambert <lee.lambert.2@au.af.edu>
2025-03-23 14:56:21 -0400

    updated to be functions that are called rather than direct stuff

--- whisper_file.py
+++ whisper_file.py
@@ -9,23 +9,50 @@ It is intended to be used to transcribe audio files and then convert the .txt fi
 From here, a LLM will be used to edit the .docx file to make it more readable.
 """
 
-model = whisper.load_model("turbo") #load whisper model
-recordings = "./Recordings" #path to audio file
-recording_text_files = "./Recording_text_files" #path to save text files
+def transcribe_audio(model, recording_path):
+    """_summary_
 
-recording_titles = os.listdir(recordings) #list of audio files
+    Args:
+        model: The Whisper model to use
 
+    Returns:
+        str: The transcribed audio text as a string 
+    """    
+    result = model.transcribe(recording_path)
+    return result
 
-for item in tqdm(recording_titles):
-    story_path = os.path.join(recordings, item)
-    
-    # STEP 1: Transcribe audio file to text; pulled from https://github.com/openai/whisper/blob/main/README.md
-    result = model.transcribe(story_path)
-    
-    # STEP 2: write the text to a .txt file
+def write_text_to_file(item, recording_text_files):
+    """_summary_
+
+    Args:
+        text: The text to write to the file
+        recording_text_files: The path to the file to write to
+        
+    Returns:
+        None
+        Saves a .txt file of the transcribed audio
+    """    
     name = item.split(".")[0]
-    with open(f"recording_text_files/{name}.txt", "w") as f:
+    with open(f"{recording_text_files}/{name}.txt", "w") as f:
         f.write(result["text"])
-    
 
-#STEP 2: Take that text and write it to a .txt file
+
+
+if __name__ == "__main__":
+
+    model = whisper.load_model("turbo") #load whisper model
+    recordings = "./Recordings" #path to audio file
+    recording_text_files = "./Recording_text_files" #path to save text files
+
+    recording_titles = os.listdir(recordings) #list of audio files
+
+    for item in tqdm(recording_titles):
+        story_path = os.path.join(recordings, item)
+        
+        # STEP 1: transcribe the audio
+        result = transcribe_audio(model, story_path)
+        
+        # STEP 2: write the text to a .txt file
+        write_text_to_file(item, recording_text_files)
+        
+        # STEP 3: convert the .txt file to a .docx file
