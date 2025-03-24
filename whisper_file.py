5acfebf3a28cb0e69b04694fbd0d5673d1ea4b0c
lee.lambert <lee.lambert.2@au.af.edu>
2025-03-23 22:51:57 -0400

    Updated to include document creation and edits. Something weird is going on in the translation. The last one I did it added a whole section and then the ai grammar and editing went bizzerk. Need to look into it.

--- whisper_file.py
+++ whisper_file.py
@@ -6,8 +6,8 @@ import ollama
 from ollama import chat
 from ollama import ChatResponse
 
-import subprocess
-import time
+from docx import Document
+
 
 """
 This project is used to transcribe audio files to text using the OpenAI Whisper model.
@@ -25,6 +25,7 @@ def transcribe_audio(model, recording_path):
     Returns:
         str: The transcribed audio text as a string 
     """    
+    print(f"Transcribing {recording_path}...")
     result = model.transcribe(recording_path)
     return result
 
@@ -39,28 +40,65 @@ def write_text_to_file(item, recording_text_files):
         None
         Saves a .txt file of the transcribed audio
     """    
+    print(f"Writing {item} to a .txt file...")
     name = item.split(".")[0]
-    with open(f"{recording_text_files}/{name}.txt", "w") as f:
-        f.write(result["text"])
+    #Need to add encoding incase the recording has special characters
+    with open(f"{recording_text_files}/{name}.txt", "w", encoding="utf-8") as f:
+        f.write(result["text"].encode('utf-8', errors='replace').decode())
 
-def correct_grammar(result):
+def correct_grammar_and_edit(item, result, recording_grammar_file_path, recording_ai_file_path):
+    print(f"Correcting the grammar in the {item} text")
+    # Chat with the Llama model to correct the grammar
     response: ChatResponse = chat(model='llama3.2', messages=[
     {
         'role': 'user',
-        'content': f'Please correct the grammar in the {result} text and reword so that it flows better as a story to captivate and to engage its reader.',
+        'content': f'Please correct the grammar, spelling and punctuation in the {result} text. Word alteration should be minimal at most. Return only the corrected text, no formatting or comments.',
     },
     ])
-    #print(response['message']['content'])
-    # or access fields directly from the response object
-    print(response.message.content)
+
+    #print(response.message.content)
+
+    # Save the corrected text to a .docx file
+    document = Document()
+    name = item.split(".")[0]
+
+    document.add_heading(f'{name}', level=1)
+    document.add_paragraph(response.message.content)
+
+    document.save(f'{recording_grammar_file_path}/{name}.docx')
+    print(f"Your text has been corrected and saved to the {name}.docx file in the {recording_grammar_file_path} folder.")
+
+    # Chat with the Llama model to edit the document
+    ai_edits(item, response, recording_ai_file_path)
+    
+def ai_edits(item, response, recording_ai_file_path):
+    print(f"Editing the {item} text with the Llama ai model")
+    new_response: ChatResponse = chat(model='llama3.2', messages=[
+    {
+        'role': 'user',
+        'content': f'You are a clarity and editing assistant. Your job is to correct grammar, punctuation, and flow while keeping the original tone and word choices as much as possible while making the writing sound more professional. Only return the corrected version of this input. Do not add commentary, summaries, introductions, or explanations. Input:{response}',
+    },
+    ])
+
+    # Save the corrected text to a .docx file
+    document = Document()
+    name = item.split(".")[0]
+
+    document.add_heading(f'{name}', level=1)
+    document.add_paragraph(new_response.message.content)
+
+    document.save(f'{recording_ai_file_path}/{name}.docx')
+    print(f"Your text has been edited and saved to the {name}.docx file in the {recording_ai_file_path} folder.")
 
 
 if __name__ == "__main__":
-
+    print("Welcome to the Whisper program! The program will transcribe your audio files to text and then correct the grammar and edit the text. It is currently loading the Whisper model and the Llama model. Please wait a moment.")
     model = whisper.load_model("turbo") #load whisper model
     recordings = "./Recordings" #path to audio file
     recording_text_files = "./Recording_text_files" #path to save text files
-
+    recording_grammar_file_path = "./Recording_docx_files_grammar_edits" #path to save corrected .docx files
+    recording_ai_file_path = "./Recording_docx_files_AI_edits" #path to save AI corrected .docx files
+    
     recording_titles = os.listdir(recordings) #list of audio files
     
     # NOTE: The Llama model is not available in the virtual environment and must be downloaded. 
@@ -89,11 +127,13 @@ if __name__ == "__main__":
         
         # STEP 1: transcribe the audio
         result = transcribe_audio(model, story_path)
-        print("Congratulations! Your audio has been transcribed.")
+        print("Congratulations! Your audio has been transcribed.\n")
         
         # STEP 2: write the text to a .txt file
         write_text_to_file(item, recording_text_files)
-        print("Your text has been written to a .txt file.")
+        print("Your text has been written to a .txt file.\n")
         
         # STEP 3: correct the grammar
-        correct_grammar(result)
+        # NOTE: The correct_grammar function will also call the ai_edits function to edit the document separately
+        correct_grammar_and_edit(item, result, recording_grammar_file_path, recording_ai_file_path)
+        print("Your text has been corrected and edited. Thanks for using the program :)\n")
